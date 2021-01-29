40d7b4ea1d2e6551bd6a128194d3b94765b3cc7a
Aadarsh <aadarsh-s@outlook.com>
2021-01-29 22:04:19 +0530

    Adding ingress load balancing service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,15 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req: express.Request, res: express.Response) => {
+  res.send({
+    data: {
+      userid: 1,
+      username: 'itsaadarsh',
+    },
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started at 3000');
 });
