400a51b58001f2a5737802a6170e69eef2a0afca
Aadarsh <aadarsh-s@outlook.com>
2021-02-10 11:52:38 +0530

    Adding index page auth logic calls

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,19 @@
-const IndexPage = () => <h1>Welcome to Seatbooked.com</h1>;
+import axios from 'axios';
+import { NextPage } from 'next';
+import axiosBuild from '../utils/buildClient';
+
+interface PROPS {
+  [key: string]: Object;
+}
+
+const IndexPage: NextPage<PROPS> = ({ currentUser }) => {
+  console.log(currentUser);
+  return <h1>Welcome to Seatbooked.com</h1>;
+};
+
+IndexPage.getInitialProps = async ({ req }) => {
+  const axiosRes = await axiosBuild(req!).get('/api/users/currentuser');
+  return axiosRes.data;
+};
 
 export default IndexPage;
