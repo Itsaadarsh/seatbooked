9a34bcaa283a550bbf3999f6143856e273f0dad9
Aadarsh <aadarsh-s@outlook.com>
2021-02-19 16:22:36 +0530

    checking path specific testing logic

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 app.listen(3000, async () => {
-  console.log('Auth service getting started......');
+  console.log('Auth service getting started......!');
   try {
     await mongoose.connect(process.env.MONGO_URI!, {
       useNewUrlParser: true,
